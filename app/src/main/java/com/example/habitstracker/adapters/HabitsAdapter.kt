1e1b2ee8fcac8289344f86d2974f2f28a69f85e1
alina <agera404gmail.com>
2022-04-19 14:16:27 +0500

    Доделка HabitsAdapter.kt

--- app/src/main/java/com/example/habitstracker/adapters/HabitsAdapter.kt
+++ app/src/main/java/com/example/habitstracker/adapters/HabitsAdapter.kt
@@ -1,25 +1,29 @@
 package com.example.habitstracker.adapters
 
-import android.view.View
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.example.habitstracker.R
+import com.example.habitstracker.databinding.HabitItemBinding
 
-class HabitsAdapter(private val dataSet: Array<Habits>): RecyclerView.Adapter<HabitsAdapter.ViewHolder>() {
-    class ViewHolder(view: View){
-        init {
+import com.example.habitstracker.models.Habit
 
+class HabitsAdapter(private val habitsSet: Array<Habit>) : RecyclerView.Adapter<HabitsAdapter.ViewHolder>() {
+    class ViewHolder(private val _binding: HabitItemBinding): RecyclerView.ViewHolder(_binding.root){
+        fun bind(habit: Habit){
+            _binding.habitItemTextView.text = habit.name
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.text_row_item, parent, false)
+        val _binding = HabitItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
 
-        return ViewHolder(view)
+        return ViewHolder(_binding)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val habit = habitsSet[position]
+        holder.bind(habit)
     }
+    override fun getItemCount(): Int = habitsSet.size
+
 }
---
+++ app/src/main/java/com/example/habitstracker/models/Habit.kt
@@ -0,0 +1,8 @@
+package com.example.habitstracker.models
+
+import java.util.*
+
+
+class Habit(var name: String, val dates: List<Date>) {
+
+}
